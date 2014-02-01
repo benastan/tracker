a87854224840c34216353ceb79c30d467503d9e7
Ben Bergstein <bennyjbergstein@gmail.com>
2014-01-31 21:52:58 -0800

    First Commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+# gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'haml-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'pg'
+gem 'inherited_resources'
+gem 'rails', '4.0.0'
+gem 'rspec-rails'
+gem 'sass-rails', '~> 4.0.0'
+gem 'simple_form'
+gem 'shoulda-matchers'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+# gem 'unicorn'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tracker::Application.config.secret_key_base = '83f18a5622078f968cb2f2aca3c5ac1563cd43b67d1761b6d6f3712f90827d382007e415729c02c812c8bfd550312bd3103ae89f5224fe3a1ffde262626208b5'
