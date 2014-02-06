17a33957202c14f5667e22940ca0cb2c116d2e4d
Ben Bergstein <bennyjbergstein@gmail.com>
2014-02-05 20:20:48 -0800

    [#4] Reorganize parent/child relationships instead of rejecting invalid configurations

--- app/models/story_story.rb
+++ app/models/story_story.rb
@@ -3,16 +3,38 @@ class StoryStory < ActiveRecord::Base
 	belongs_to :child_story, class_name: :Story
   validates :parent_story_id, :child_story_id, presence: true
   validates :parent_story_id, uniqueness: { scope: :child_story_id }
-  
-  validate do
-    if parent_story_id == child_story_id
-      errors.add(:parent_story_id, 'cannot be its own parent')
+
+  before_create do
+    invalid_parent_story_stories = StoryStory.parent_story_stories_of(parent_story).where(parent_story: child_story)
+
+    if invalid_parent_story_stories.any?
+      invalid_parent_story_stories.destroy_all
     end
+
+    true
   end
 
   def serializable_hash(options = {})
     options[:include] ||= []
     options[:include] << :parent_story
     super(options)
+  end
+
+  class << self
+    def parent_story_stories_of(story)
+      where("id in (#{parent_story_stories_sql_for(story.id)})")
+    end
+
+    def parent_story_stories_sql_for(story_id)
+      <<-SQL
+WITH RECURSIVE parent_stories(id, parent_story_id, child_story_id) AS (
+    SELECT id, parent_story_id, child_story_id FROM story_stories WHERE child_story_id = #{story_id}
+  UNION ALL
+    SELECT ss.id, ss.parent_story_id, ss.child_story_id
+    FROM parent_stories ps, story_stories ss
+    WHERE ps.parent_story_id = ss.child_story_id
+) SELECT id from parent_stories
+SQL
+    end
   end
 end
